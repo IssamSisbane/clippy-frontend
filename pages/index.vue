2923af70a222e031d057095d09bf640987c52b71
Issam <issam.sisbane@gmail.com>
2024-02-22 21:43:12 +0100

    add buttons to index page

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <AppHeader />
+
+  <div class="flex items-center justify-center space-x-6">
+    <UButton size="xl" variant="soft">
+      <NuxtLink to="/add">
+        Add
+      </NuxtLink>
+    </UButton>
+    <UButton size="xl" variant="soft">
+      <NuxtLink to="/find">
+        Find
+      </NuxtLink>
+    </UButton>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style></style>
