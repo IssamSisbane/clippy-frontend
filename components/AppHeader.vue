848e9eb8b4c70be40e286799345180f9e912363c
Issam <issam.sisbane@gmail.com>
2024-02-22 21:42:18 +0100

    add header component

---
+++ components/AppHeader.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="flex flex-col items-center p-8">
+        <h1 class="text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl border-b-2 mb-3 pb-4">
+            <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Clippy</span>
+        </h1>
+    </div>
+</template>
