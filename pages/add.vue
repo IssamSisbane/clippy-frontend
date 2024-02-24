7725289e3722b91ad60af3f0255457c19c21f355
Issam <issam.sisbane@gmail.com>
2024-02-24 22:31:46 +0100

    add other form element to add a new clip

--- pages/add.vue
+++ pages/add.vue
@@ -2,12 +2,30 @@
   <AppHeader />
   <div class="min-w-64 w-1/2 md:w-1/3 mx-auto">
     <UForm ref="form" :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
-      <UFormGroup name="input" label="Input">
-        <UInput v-model="state.input" />
+      <UFormGroup name="title" label="Title">
+        <UInput v-model="state.title" />
       </UFormGroup>
 
-      <UFormGroup name="textarea" label="Textarea">
-        <UTextarea v-model="state.textarea" />
+
+      <UFormGroup name="content" label="Content" v-if="state.type.value == ClipTypeEnum.Text">
+        <UTextarea v-model="state.content" />
+      </UFormGroup>
+
+      <UFormGroup name="content" label="Content" v-if="state.type.value == ClipTypeEnum.Image">
+        <UInput type="file" size="sm" />
+      </UFormGroup>
+
+      <UFormGroup name="content" label="Content" v-if="state.type.value == ClipTypeEnum.File">
+        <UInput v-model="state.file" type="file" size="sm" />
+      </UFormGroup>
+
+      <UFormGroup name="selectMenuTypeOption" label="Select Type option">
+        <USelectMenu v-model="state.type" placeholder="Select..." :options="typeOptions" />
+
+      </UFormGroup>
+
+      <UFormGroup name="selectMenuTimeToLive" label="Select TimeToLive option">
+        <USelectMenu v-model="state.timeToLive" placeholder="Select..." :options="ttlOptions" icon="i-heroicons-clock" />
       </UFormGroup>
 
       <div class="flex justify-center">
@@ -27,14 +45,32 @@ import { z } from 'zod'
 import type { FormSubmitEvent } from '#ui/types'
 import { ClipTypeEnum, TttEnum, type Clip } from '~/models/clip.model';
 
+const typeOptions = [
+  { label: 'Text', value: ClipTypeEnum.Text },
+  { label: 'Image', value: ClipTypeEnum.Image },
+  { label: 'File', value: ClipTypeEnum.File }
+]
+
+const ttlOptions = [
+  { label: 'A Day', value: TttEnum.Day },
+  { label: 'A Week', value: TttEnum.Week },
+  { label: 'A Month', value: TttEnum.Month }
+]
+
 const state = reactive({
-  input: undefined,
-  textarea: undefined,
+  title: undefined,
+  content: undefined,
+  file: undefined,
+  type: typeOptions[0],
+  timeToLive: ttlOptions[0],
 })
 
 const schema = z.object({
-  input: z.string().min(1),
-  textarea: z.string().min(1)
+  title: z.string().min(1),
+  content: z.string().min(1),
+  file: z.any(),
+  type: z.any(),
+  timeToLive: z.any(),
 })
 
 type Schema = z.infer<typeof schema>
@@ -43,10 +79,11 @@ const form = ref();
 let submitError = ref();
 
 async function onSubmit(event: FormSubmitEvent<Schema>) {
+  console.log("submit")
   const clip: Clip = {
     path: 'test',
-    title: event.data.input,
-    content: event.data.textarea,
+    title: event.data.title,
+    content: event.data.content,
     type: ClipTypeEnum.Text,
     timeToLive: TttEnum.Day,
   };
