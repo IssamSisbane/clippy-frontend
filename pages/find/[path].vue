f9d079e70677058ba27bd9036ee43831b11189db
Issam <issam.sisbane@gmail.com>
2024-03-02 18:36:31 +0100

    add nuxt image

--- pages/find/[path].vue
+++ pages/find/[path].vue
@@ -5,7 +5,7 @@
     <h1 class="text-3xl pb-8 text-center">Clip not found</h1>
   </div>
   <div v-else class="flex flex-col items-center">
-    <h1 class="text-4xl pb-4">{{ fullClipUrl }}</h1>
+    <h1 class="text-xl lg:text-4xl pb-4">{{ fullClipUrl }}</h1>
     <UButton class="mb-4" icon="i-heroicons-clipboard-document" size="sm" color="primary" square variant="soft"
       @click="copy">
     </UButton>
@@ -13,19 +13,19 @@
 
     <UCard>
       <template #header>
-        <div class="w-64">
+        <div class="w-100">
           <h1 class="text-2xl text-center">{{ clip!.title }}</h1>
         </div>
 
       </template>
 
-      <div class="w-64">
+      <div class="w-100">
         <p>{{ clip!.content }}</p>
       </div>
 
       <template #footer>
-        <div class="w-64">
-          <img :src="clip!.file" alt="clip" />
+        <div class="w-100">
+          <NuxtImg :src="clip!.file" alt="clip.file" />
         </div>
       </template>
 
